18105eddf39faa8e31a4357dde7c58342265b5b6
Ritik Mehta <ritikmehta799@gmail.com>
2023-02-06 15:13:25 +0530

    React

---
+++ React Js/mainrouter.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter} from "react-router-dom";
+import Header from './Componet/header'
+import Home from './Home'
+import About from './About'
+
+const Mainrouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Header/>,
+    },
+    {
+      path: "/About",
+      element: <About/>,
+    },
+  ]);
+
+  export default Mainrouter;
