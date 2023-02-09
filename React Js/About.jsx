5627c94de6a42c69b9bdb36fc33524e0300a0693
Ritik Mehta <ritikmehta799@gmail.com>
2023-02-09 19:46:06 +0530

    React

--- React Js/About.jsx
+++ React Js/About.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
 
 class About extends Component {
     render() {
         return (
-            <div>
-                know more about
+            <div className='container'>
+                <div className="row">
+                    <div className="col-3">
+                        know more about us
+                        <br />
+                       <a href="#">Follow us</a> 
+                    </div>
+                </div>
+
             </div>
         );
     }
