f8d212da9c65eac266190bd0559d4b4ab1ba40d2
Ritik Mehta <ritikmehta799@gmail.com>
2023-03-06 19:43:55 +0530

    React

--- React Js/src/Subscribe.jsx
+++ React Js/src/Subscribe.jsx
@@ -8,18 +8,49 @@ const Subscribe = () => {
                 <div className="row">
                     <div className="col">
                         <h3>Subscribe to watch all content on Disney+ Hotstar</h3>
-                        {/* <div class="img-wrapper" style="margin-top: 20px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/1213/871213-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/2445/1202445-v-25736a852683" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/5150/875150-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/5239/875239-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/1657/421657-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/7539/617539-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/3675/1213675-v-988bcccda92a" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/9263/1089263-v-13f4d7e9411b" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/8195/858195-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/2155/912155-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/5180/875180-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"></div>
-                    
-                        <div class="img-wrapper" style="margin-top: 20px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/5834/575834-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/2606/352606-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/3589/763589-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/5199/875199-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/6699/1026699-v-cb786ee970de" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/8442/1478442-v-c0f317dcf6a8" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/4719/994719-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/3145/813145-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/1213/871213-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/2445/1202445-v-25736a852683" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"><img class="tray-img" src="https://img1.hotstarext.com/image/upload/f_auto,t_web_vl_1_5x/sources/r1/cms/prod/5150/875150-v" alt="trayimg" style="max-width: 119.636px; max-height: 157.92px; margin: 0px 10px;"></div> */}
-                    <div className="box">
-                        <div className="space"></div>
-                        <div className="super">
-                            <span>Super</span>
+                        <div className="box">
+                            <table>
+                                <tr>
+                                    {/* <td></td> */}
+                                    <td colSpan={1}></td>
+                                    <td className='mid back-chnge clr-chng'> Super</td>
+                                    <td className='mid'>Premium</td>
+                                </tr>
+                                <tr>
+                                    <td rowSpan={1} className='lef'>All contect
+                                        <br />
+                                        <span className='movie'>Movies, live sports, TV, Specials</span>
+                                    </td>
+                                    <td className='back-chnge'><i class="fa-solid fa-check"></i></td>
+                                    <td ><i class="fa-solid fa-check"></i></td>
+                                </tr>
+                                <tr>
+                                    <td className='lef'>Watch on TV or Laptop</td>
+                                    <td className='back-chnge'><i class="fa-solid fa-check"></i></td>
+                                    <td ><i class="fa-solid fa-check"></i></td>
+                                </tr>
+                                <tr>
+                                    <td className='lef'>Ads free movies and shows (except sports)</td>
+                                    <td className='back-chnge'><i class="fa-solid fa-xmark"></i></td>
+                                    <td ><i class="fa-solid fa-check"></i></td>
+                                </tr>
+                                <tr>
+                                    <td className='lef'>Number of devices that can be logged in</td>
+                                    <td className='back-chnge'>2</td>
+                                    <td >4</td>
+                                </tr>
+                                <tr>
+                                    <td className='lef'>Max video quality</td>
+                                    <td className='back-chnge'>Full HD(1080p)</td>
+                                    <td >4K (2160p)</td>
+                                </tr>
+                                <tr>
+                                    <td className='lef'>Max audio quality</td>
+                                    <td className='back-chnge bor-rad'>Dolby 5.1</td>
+                                    <td >Dolby 5.1</td>
+                                </tr>
+                            </table>
                         </div>
-                        <div className="premium">
-                            <span>Premium</span>
-                        </div>
-                    </div>
                     </div>
                 </div>
             </div>
