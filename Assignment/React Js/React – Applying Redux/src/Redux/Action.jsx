9fba5031fe1ed1af11dce6340027b7ad13876965
Ritik Mehta <ritikmehta799@gmail.com>
2023-05-08 23:40:15 +0530

    Assignment

--- Assignment/React Js/React – Applying Redux/src/Redux/Action.jsx
+++ Assignment/React Js/React – Applying Redux/src/Redux/Action.jsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { FAIL_REQUEST, GET_USER_LIST, MAKE_REQUEST } from "./Actiontype"
+import { DELETE_USER_, FAIL_REQUEST, GET_USER_LIST, MAKE_REQUEST } from "./Actiontype"
 
 export const makeRequest= () => {
     return{
@@ -18,11 +18,17 @@ export const getUserList= (data) => {
         payload:data
     }
 }
+export const deleteUser= () => {
+    return{
+        type:DELETE_USER_
+
+    }
+}
 
 export const  FeatchUserList=()=>{
     return (dispatch)=>{
         dispatch(makeRequest());
-        axios.get('http://localhost:5000/user').then(res=>{
+        axios.get('http://localhost:3001/user').then(res=>{
             const userlist=res.data;
             dispatch(getUserList(userlist))
         }).catch(err=>{
