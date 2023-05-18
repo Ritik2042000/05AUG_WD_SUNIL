ca2835c2d84c63e2f6e279183f3bacc885e0057d
Ritik Mehta <ritikmehta799@gmail.com>
2023-05-18 23:57:58 +0530

    Projects

--- React Js/Projects/src/Components/Weather-app/WeatherCard.jsx
+++ React Js/Projects/src/Components/Weather-app/WeatherCard.jsx
@@ -1,38 +1,38 @@
 import React, { useEffect, useState } from 'react';
 
-const WeatherCard = ({info}) => {
-    const [weatherIcon,setWeatherIcon] =useState()
-    
+const WeatherCard = ({ info }) => {
+    const [weatherIcon, setWeatherIcon] = useState()
+
     const {
         temp, humidity, pressure, weathermood, name, country, sunset, speed
-    }= info
-    
-    let sec =sunset;
-    let date = new Date (sec * 1000 );
+    } = info
+
+    let sec = sunset;
+    let date = new Date(sec * 1000);
     let timeSunset = `${date.getHours()}:${date.getMinutes()}`
 
     useEffect(() => {
         if (weathermood) {
-          switch (weathermood) {
-            case "Clouds":
-              setWeatherIcon("wi-day-cloudy");
-              break;
-            case "Haze":
-              setWeatherIcon("wi-fog");
-              break;
-            case "Clear":
-              setWeatherIcon("wi-day-sunny");
-              break;
-            case "Mist":
-              setWeatherIcon("wi-dust");
-              break;
-    
-            default:
-              setWeatherIcon("wi-day-sunny");
-              break;
-          }
+            switch (weathermood) {
+                case "Clouds":
+                    setWeatherIcon("wi-day-cloudy");
+                    break;
+                case "Haze":
+                    setWeatherIcon("wi-fog");
+                    break;
+                case "Clear":
+                    setWeatherIcon("wi-day-sunny");
+                    break;
+                case "Mist":
+                    setWeatherIcon("wi-dust");
+                    break;
+
+                default:
+                    setWeatherIcon("wi-day-sunny");
+                    break;
+            }
         }
-      }, [weathermood]);
+    }, [weathermood]);
     return (
         <>
             <article className='widget'>
@@ -60,7 +60,7 @@ const WeatherCard = ({info}) => {
                                 <i className={'wi wi-sunset'}></i>
                             </p>
                             <p className="extra-info-leftside">
-                            {timeSunset} PM<br />
+                                {timeSunset} PM<br />
                                 Sunset
                             </p>
                         </div>
@@ -70,8 +70,8 @@ const WeatherCard = ({info}) => {
                                 <i className={'wi wi-humidity'}></i>
                             </p>
                             <p className="extra-info-leftside">
-                            {humidity} %<br />
-                            Humidity
+                                {humidity} %<br />
+                                Humidity
                             </p>
                         </div>
                     </div>
@@ -81,7 +81,7 @@ const WeatherCard = ({info}) => {
                                 <i className={'wi wi-rain'}></i>
                             </p>
                             <p className="extra-info-leftside">
-                            {pressure} P.A<br />
+                                {pressure} P.A<br />
                                 Pressure
                             </p>
                         </div>
@@ -90,8 +90,8 @@ const WeatherCard = ({info}) => {
                                 <i className={'wi wi-strong-wind'}></i>
                             </p>
                             <p className="extra-info-leftside">
-                            {speed} k.m <br />
-                               Wind 
+                                {speed} k.m <br />
+                                Wind
                             </p>
                         </div>
                     </div>
