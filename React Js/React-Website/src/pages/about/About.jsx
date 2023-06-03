08f3208ed4a6ffd5bbaebfa53c4baafa797f6808
Ritik Mehta <ritikmehta799@gmail.com>
2023-06-03 19:40:22 +0530

    React-Website

--- React Js/React-Website/src/pages/about/About.jsx
+++ React Js/React-Website/src/pages/about/About.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import  './about.css';
-import { GoGrabber } from "react-icons/go"
+
 
 const About = () => {
     return (
         <div>
             about
             b
-            <button>{GoGrabber}</button>
+            
         </div>
     );
 };
