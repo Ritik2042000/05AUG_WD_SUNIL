d5ee51f262fea14ee7ff49537c4580097a665634
Ritik2042000 <ritikmehta799@gmail.com>
2023-07-11 20:26:28 +0530

    ecommerce-app

--- React Js/ecommerce/src/Context/FilterContex.jsx
+++ React Js/ecommerce/src/Context/FilterContex.jsx
@@ -9,27 +9,41 @@ const initialState = {
     filter_products: [],
     all_products: [],
     grid_view: true,
+    sorting_value: 'lowest',
 };
 
 export const FilterContextProvider = ({ children }) => {
     const { products } = useProductContext()
-    console.log(products);
-    
+    // console.log(products);
+
     const [state, dispatch] = useReducer(reducer, initialState);
-    
+
     // for grid view
     const setGridView = () => {
-        return dispatch({ type: 'SET_GRIDVIEW' });
+        return dispatch({ type: 'SET_GRID_VIEW' });
     }
+
+    // for list view
+    const setListView = () => {
+        return dispatch({ type: 'SET_LIST_VIEW' });
+    }
+    const sortingItems = () => {
+        return dispatch({ type: 'GET_SORT_VALUE' });
+    }
+
+    useEffect(() => {
+        dispatch({ type: 'SORTING_PRODUCTS', payload: products });
+    }, [state.sorting_value])
     
     // to load all the products for grid and list view
     useEffect(() => {
         dispatch({ type: 'LOAD_FILTER_PRODUCTS', payload: products });
-        console.log(products ,'in usa effect');
+        // console.log(products, 'in use effect');
     }, [products])
+    
 
     return (
-        <FilterContex.Provider value={{ ...state, setGridView }}>
+        <FilterContex.Provider value={{ ...state, setGridView,setListView,sortingItems }}>
             {children}
         </FilterContex.Provider>
     );
--- React Js/ecommerce/src/Reducer/filterReducer.jsx
+++ React Js/ecommerce/src/Reducer/filterReducer.jsx
@@ -10,11 +10,59 @@ const filterReducer = (state, action) => {
                 all_products: [...action.payload],
 
             }
-           
-        case 'SET_GRIDVIEW':
+
+        case 'SET_GRID_VIEW':
             return {
                 ...state,
                 grid_view: true,
+
+            }
+
+        case 'SET_LIST_VIEW':
+            return {
+                ...state,
+                grid_view: false,
+            }
+
+        case 'GET_SORT_VALUE':
+            let userSortValue = document.getElementById('sort');
+            let sort_value = userSortValue.options[userSortValue.selectedIndex].value
+            console.log(sort_value);
+            return {
+                ...state,
+                sorting_value: sort_value
+            }
+
+        case 'SORTING_PRODUCTS':
+            let newSortData;
+            let temSortProduct = [...action.payload];
+
+            if (state.sorting_value === 'lowest') {
+                const sortProductsByPrice = (a, b) => {
+                    return a.price - b.price;
+                }
+                newSortData = temSortProduct.sort(sortProductsByPrice)
+            }
+            if (state.sorting_value === 'highest') {
+                const sortProductsByPrice = (a, b) => {
+                    return b.price - a.price;
+                }
+                newSortData = temSortProduct.sort(sortProductsByPrice)
+            }
+            if (state.sorting_value === 'a-z') {
+                newSortData = temSortProduct.sort((a, b) =>
+                    a.name.localeCompare(b.name)
+                );
+            }
+            if (state.sorting_value === 'z-a') {
+                newSortData = temSortProduct.sort((a, b) =>
+                    b.name.localeCompare(a.name)
+                );
+            }
+
+            return {
+                ...state,
+                filter_products: newSortData,
             }
 
         default:
