d0b9c46ebed7cb034b0c494ec238923c1b3305f8
Ritik2042000 <ritikmehta799@gmail.com>
2023-08-04 23:57:16 +0530

    Js

---
+++ Javascript/Countdown Timer/index.js
@@ -0,0 +1,60 @@
+(function () {
+    var hours = document.querySelector(".hour");
+    var min = document.querySelector(".minute");
+    var sec = document.querySelector(".sec");
+
+    var startbtn = document.querySelector(".start");
+    var stopbtn = document.querySelector(".stop");
+    var resetBtn = document.querySelector(".reset");
+
+    var countdownTimer = null;
+
+    startbtn.addEventListener('click',function () {
+        if(hours.value == 0 && min.value == 0 && sec.value == 0) return;
+
+    function startInterval() {
+        startbtn.style.display='none';
+        stopbtn.style.display='initial';
+
+        countdownTimer = setInterval(()=>{
+            timer()
+        },1000)
+    }
+
+    startInterval();
+    });
+
+        function stopInterval(params) {
+            
+        }
+
+
+    function timer() {
+
+        if(sec.value > 60){
+            min.value++;
+            sec.value = parseInt(sec.value)-59
+        }
+        if(min.value > 60){
+            hours.value++;
+            min.value = parseInt(min.value)-59
+        }
+        if(hours.value == 0 && min.value == 0 && sec.value == 0){
+            hours.value='';
+            min.value='';
+            sec.value='';
+            stopInterval()
+        }
+        else if(sec.value != 0){
+            sec.value = `${sec.value <=10 ? '0' : ''}${sec.value - 1}`
+        } else if(min.value != 0 && sec.value == 0){
+            sec.value = 59;
+            min.value = `${min.value <=10 ? '0' : ''}${min.value - 1}`;
+        }
+         else if(hours.value != 0 && min.value == 0){
+            min.value = 59;
+            hours.value = `${hours.value <=10 ? '0' : ''}${hours.value - 1}`
+
+        }
+    }
+})();
