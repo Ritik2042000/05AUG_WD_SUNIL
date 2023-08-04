3ac2af197df09df4934d32927e96c4cee9967aa0
Ritik2042000 <ritikmehta799@gmail.com>
2023-08-05 00:25:54 +0530

    Js

--- Javascript/Countdown Timer/index.js
+++ Javascript/Countdown Timer/index.js
@@ -9,52 +9,68 @@
 
     var countdownTimer = null;
 
-    startbtn.addEventListener('click',function () {
-        if(hours.value == 0 && min.value == 0 && sec.value == 0) return;
+    startbtn.addEventListener('click', function () {
+        if (hours.value == 0 && min.value == 0 && sec.value == 0) return;
 
-    function startInterval() {
-        startbtn.style.display='none';
-        stopbtn.style.display='initial';
+        function startInterval() {
+            startbtn.style.display = 'none';
+            stopbtn.style.display = 'initial';
 
-        countdownTimer = setInterval(()=>{
-            timer()
-        },1000)
-    }
+            countdownTimer = setInterval(() => {
+                timer()
+            }, 1000)
+        }
 
-    startInterval();
+        startInterval();
     });
 
-        function stopInterval(params) {
-            
-        }
+    function stopInterval(state) {
+        startbtn.innerHTML = state === 'pause' ? 'Countine' : 'Start';
+
+        startbtn.style.display = 'initial';
+        stopbtn.style.display = 'none';
+        clearInterval(countdownTimer)
+
+    }
 
 
     function timer() {
 
-        if(sec.value > 60){
+        if (sec.value > 60) {
             min.value++;
-            sec.value = parseInt(sec.value)-59
+            sec.value = parseInt(sec.value) - 59
         }
-        if(min.value > 60){
+        if (min.value > 60) {
             hours.value++;
-            min.value = parseInt(min.value)-59
+            min.value = parseInt(min.value) - 60
         }
-        if(hours.value == 0 && min.value == 0 && sec.value == 0){
-            hours.value='';
-            min.value='';
-            sec.value='';
+        if (hours.value == 0 && min.value == 0 && sec.value == 0) {
+            hours.value = '';
+            min.value = '';
+            sec.value = '';
             stopInterval()
         }
-        else if(sec.value != 0){
-            sec.value = `${sec.value <=10 ? '0' : ''}${sec.value - 1}`
-        } else if(min.value != 0 && sec.value == 0){
+        else if (sec.value != 0) {
+            sec.value = `${sec.value <= 10 ? '0' : ''}${sec.value - 1}`
+        } else if (min.value != 0 && sec.value == 0) {
             sec.value = 59;
-            min.value = `${min.value <=10 ? '0' : ''}${min.value - 1}`;
+            min.value = `${min.value <= 10 ? '0' : ''}${min.value - 1}`;
         }
-         else if(hours.value != 0 && min.value == 0){
+        else if (hours.value != 0 && min.value == 0) {
             min.value = 59;
-            hours.value = `${hours.value <=10 ? '0' : ''}${hours.value - 1}`
-
+            hours.value = `${hours.value <= 10 ? '0' : ''}${hours.value - 1}`
         }
     }
+
+    stopbtn.addEventListener('click', function () {
+        stopInterval('pause')
+    })
+
+    resetBtn.addEventListener('click',function(){
+        sec.value= '';
+        min.value= '';
+        hours.value= '';
+
+        stopInterval()
+    })
 })();
