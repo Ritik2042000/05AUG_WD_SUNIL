bfefb1558a57d26802d0150e137b0b55294a6a7b
Ritik2042000 <ritikmehta799@gmail.com>
2024-08-27 20:45:20 +0530

    js_Ecom

--- Javascript/Ecommerece/homeProductCards.js
+++ Javascript/Ecommerece/homeProductCards.js
@@ -1,3 +1,5 @@
+import { homeQuantityToggle } from "./homeQuantityToggle";
+
 const producatContainer = document.querySelector('#productContainer');
 const productTemplate = document.querySelector('#productTemplate')
 
@@ -10,14 +12,19 @@ export const showProductContainer = (products) => {
         const {id,name,brand,category,description,image,price,stock} = data;
         
         const productClone = document.importNode(productTemplate.content,true);
+        productClone.querySelector('#cardValue').setAttribute("id",`card${id}`);
         productClone.querySelector('.productName').textContent = name;
         productClone.querySelector('.category').textContent = category;
         productClone.querySelector('.productImage').src = image;
         productClone.querySelector('.productImage').alt = name;
         productClone.querySelector('.productStock').textContent = stock;
         productClone.querySelector('.productDescription').textContent = description;
+        productClone.querySelector('.productPrice').textContent = `₹${price}`;
+        productClone.querySelector('.productActualPrice').textContent = `₹${price * 5}`;
 
-
+         productClone.querySelector('.stockElement').addEventListener(('click'),(event) =>{
+            homeQuantityToggle(event,id,stock) 
+         } )
         producatContainer.append(productClone)
     
     });
