9d5ac05c4548e728ef6954bf7fd5c8f77a91807f
Ritik2042000 <ritikmehta799@gmail.com>
2024-10-31 19:15:50 +0530

    Next Js Project

--- Next Js/Projects/patientApp/components/forms/RegisterForm.tsx
+++ Next Js/Projects/patientApp/components/forms/RegisterForm.tsx
@@ -9,7 +9,7 @@ import CustomFormField from "../ui/CustomFormField";
 import { useState } from "react";
 import { PatientFormValidation, UserFormValidation } from "@/lib/Validation";
 import { useRouter } from "next/navigation";
-import { createUser } from "@/lib/actions/patient.actions";
+import { createUser, registerPatient } from "@/lib/actions/patient.actions";
 import SubmitButton from "../SubmitButton";
 import { FormFieldType } from "./PatinetForm";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
@@ -23,6 +23,7 @@ import { Label } from "../ui/label";
 import { SelectItem } from "../ui/select";
 import Image from "next/image";
 import FileUploader from "../ui/FileUploader";
+import { register } from "module";
 
 const RegisterForm = ({ user }: { user: User }) => {
   const router = useRouter();
@@ -43,17 +44,35 @@ const RegisterForm = ({ user }: { user: User }) => {
   const onSubmit = async (values: z.infer<typeof PatientFormValidation>) => {
     setIsLoading(true);
 
+    let formData;
+
+    if (
+      values.identificationDocument &&
+      values.identificationDocument.length > 0
+    ) {
+      const blobFile = new Blob([values.identificationDocument[0]], {
+        type: values.identificationDocument[0].type,
+      });
+      formData = new FormData();
+      formData.append("blobFile", blobFile);
+      formData.append("fileName", values.identificationDocument[0].name);
+    }
     try {
-      const user = {
+      const patientData = {
+        ...values,
+        userId: user.$id,
         name: values.name,
+        birthDate: new Date(values.birthDate),
+        identificationDocument: formData,
         email: values.email,
         phone: values.phone,
       };
 
-      const newUser = await createUser(user);
+      //@ts-ignore
+      const patient = await registerPatient(patientData);
 
-      if (newUser) {
-        router.push(`/patients/${newUser.$id}/register`);
+      if (patient) {
+        router.push(`/patients/${user.$id}/new-appointment`);
       }
     } catch (error) {
       console.log(error);
