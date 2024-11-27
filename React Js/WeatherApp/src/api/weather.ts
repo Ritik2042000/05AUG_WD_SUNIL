46407c2a37250aaa18e53dda6a2e0563ddf50668
Ritik2042000 <ritikmehta799@gmail.com>
2024-11-27 20:42:40 +0530

    Wether App Project

--- React Js/WeatherApp/src/api/weather.ts
+++ React Js/WeatherApp/src/api/weather.ts
@@ -1,5 +1,10 @@
 import { API_CONFIG } from "./config";
-import { Coordinates, ForecastData, GeocondingResponse, WeatherData } from "./type";
+import {
+  Coordinates,
+  ForecastData,
+  GeocondingResponse,
+  WeatherData,
+} from "./type";
 
 class WeatherAPI {
   private createUrl(endpoint: string, params: Record<string, string | number>) {
@@ -26,25 +31,32 @@ class WeatherAPI {
       lon: lon.toString(),
       units: API_CONFIG.DEFAULT_PARAMS.units,
     });
+    console.log(url, "inside cueWer");
     return this.fetchData<WeatherData>(url);
   }
+
   async getForecast({ lat, lon }: Coordinates): Promise<ForecastData> {
     const url = this.createUrl(`${API_CONFIG.BASE_URL}/forecast`, {
       lat: lat.toString(),
       lon: lon.toString(),
       units: API_CONFIG.DEFAULT_PARAMS.units,
     });
+    console.log(url, "inside forcast");
     return this.fetchData<ForecastData>(url);
   }
 
-  async reverseGeocode({ lat, lon }: Coordinates): Promise<GeocondingResponse[]> {
+  async reverseGeocode({
+    lat,
+    lon,
+  }: Coordinates): Promise<GeocondingResponse[]> {
     const url = this.createUrl(`${API_CONFIG.GEO}/reverse`, {
       lat: lat.toString(),
       lon: lon.toString(),
-      limit:1,
+      limit: 1,
     });
+    console.log(url, "inside geo");
     return this.fetchData<GeocondingResponse[]>(url);
   }
 }
 
-export const weatherAPI = new WeatherAPI() 
+export const weatherAPI = new WeatherAPI();
--- React Js/WeatherApp/src/pages/WeatherDashboard.tsx
+++ React Js/WeatherApp/src/pages/WeatherDashboard.tsx
@@ -1,7 +1,13 @@
-import { RefreshCcw } from "lucide-react";
+import { AlertTriangle, MapPin, RefreshCcw } from "lucide-react";
 import { Button } from "../components/ui/button";
 import { useGeolocation } from "@/hooks/use-geolocation";
 import { WeatherSkeletion } from "@/components/loading-skeleton";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import {
+  useForecastQuery,
+  useReverseGeocodeQuery,
+  useWeatherQuery,
+} from "@/hooks/use-Weather";
 
 const WeatherDashboard = () => {
   const {
@@ -11,15 +17,77 @@ const WeatherDashboard = () => {
     isLoading: locationLoading,
   } = useGeolocation();
 
+  const weatherQuery = useWeatherQuery(coordinates);
+  const forecastQuery = useForecastQuery(coordinates);
+  const locationQuery = useReverseGeocodeQuery(coordinates);
+
+  // log
+
   const handleRefresh = () => {
     getLocation();
     if (coordinates) {
-      //
+      weatherQuery.refetch();
+      forecastQuery.refetch();
+      locationQuery.refetch();
     }
   };
 
   if (locationLoading) {
     return <WeatherSkeletion />;
+  }
+
+  if (locationError) {
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle />
+        <AlertTitle>Location Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>{locationError}</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="h-4 mr-2 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (!coordinates) {
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle />
+        <AlertTitle>Location Required</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Please Enable Your Location Access To See Your Local Weather</p>
+          <Button onClick={getLocation} variant={"outline"} className="w-fit">
+            <MapPin className="h-4 mr-2 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  const locationName = locationQuery.data?.[0];
+
+  if (weatherQuery.error || forecastQuery.error) {
+    return (
+      <Alert variant="destructive">
+        <AlertTriangle />
+        <AlertTitle> Error</AlertTitle>
+        <AlertDescription className="flex flex-col gap-4">
+          <p>Failed To Fetch Weather Data.Please Try again. </p>
+          <Button onClick={handleRefresh} variant={"outline"} className="w-fit">
+            <MapPin className="h-4 mr-2 w-4" />
+            Enable Location
+          </Button>
+        </AlertDescription>
+      </Alert>
+    );
+  }
+
+  if (!weatherQuery.data || !forecastQuery.data) {
+    return <WeatherSkeletion/>
   }
 
   return (
@@ -30,9 +98,11 @@ const WeatherDashboard = () => {
           variant={"outline"}
           size={"icon"}
           onClick={handleRefresh}
-          // disabled={}
+          disabled={weatherQuery.isFetching || forecastQuery.isFetching}
         >
-          <RefreshCcw />
+          <RefreshCcw
+            className={`w-4 h-4 ${weatherQuery.isFetching && "animate-spin"}`}
+          />
         </Button>
       </div>
     </div>
