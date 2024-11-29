f231ad403c2bc624fc4521d6d17b3f8032d4b7fc
Ritik2042000 <ritikmehta799@gmail.com>
2024-11-29 20:45:09 +0530

    zentry

---
+++ React Js/zentryAnimated/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Hero from './components/Hero'
+
+
+function App() {
+
+
+  return (
+    <main className='relative h-dvh w-screen overflow-x-hidden'>
+      <Hero />
+    </main>
+  )
+}
+
+export default App
